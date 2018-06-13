c19b943091e46b38788a33b3cb73f6b47a6c11a2
Brent <bingr001@gmail.com>
2018-06-13 11:47:06 -0700

    first initializing for a bunch of changes
    37,613 changes from nom install I think
    or just the constants/apikeys

---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+	FirebaseConfig: {
+		apiKey: "AIzaSyAdUP3Kxq47BH_GRX3maciK1fvzxlaU2wo",
+    authDomain: "rlay-abdb0.firebaseapp.com",
+    databaseURL: "https://rlay-abdb0.firebaseio.com",
+    projectId: "rlay-abdb0",
+    storageBucket: "rlay-abdb0.appspot.com",
+    messagingSenderId: "902449039485"
+	}
+}
