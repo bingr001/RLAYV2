efe049b67a482e3bc32699a3946b70cc3c09919b
Brent <bingr001@gmail.com>
2018-07-09 19:09:46 -0700

    styled schedule

--- screens/Schedule.js
+++ screens/Schedule.js
@@ -1,98 +1,81 @@
 import React from 'react';
-import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View, TextInput, Button } from 'react-native';
-import { TestComponent } from './../components/AppComponents';
-import * as firebase from 'firebase';
-import { connect } from 'react-redux';
-import { event1, event2, event3, event4,
-  watchPersonData
-        } from './../redux/app-redux';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 
-const mapStateToProps1 = (state) => {
-  return {
-
-    personData: state.personData,
-    event1: state.event1,
-    event2: state.event2,
-    event3: state.event3,
-    event4: state.event4,
-  };
-}
-
-
-const mapDispatchToProps1 = (dispatch) => {
-  return {
-    setanswer1: (text) => { dispatch(setanswer1(text)) },
-    setanswer2: (text) => { dispatch(setanswer2(text)) },
-    setanswer3: (text) => { dispatch(setanswer3(text)) },
-    setanswer4: (text) => { dispatch(setanswer4(text)) },
-    setanswer5: (text) => { dispatch(setanswer5(text)) },
-    setanswer6: (text) => { dispatch(setanswer6(text)) },
-
-
-    watchPersonData: () => { dispatch(watchPersonData()) },
-  };
-}
-
-class Devo extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      answer1: this.props.answer1,
-    }
-
-    this.props.watchPersonData();
-  }
-
-  onSignoutPress = () => {
-    firebase.auth().signOut();
-  }
-
-  onSetFavoriteAnimalPress1 = () => {
-    this.props.setanswer1(this.state.answer1);
-  }
-  onSetFavoriteAnimalPress2 = () => {
-    this.props.setanswer2(this.state.answer2);
-  }
-  onSetFavoriteAnimalPress3 = () => {
-    this.props.setanswer3(this.state.answer3);
-  }
-  onSetFavoriteAnimalPress4 = () => {
-    this.props.setanswer4(this.state.answer4);
-  }
-  onSetFavoriteAnimalPress5 = () => {
-    this.props.setanswer5(this.state.answer5);
-  }
-  onSetFavoriteAnimalPress6 = () => {
-    this.props.setanswer6(this.state.answer6);
-  }
-
-
+class Schedule extends React.Component {
   render() {
-    return (
+    return <ScrollView style={{ flex: 1, padding: 20, paddingTop: 20 }}>
+        <Text style={{ fontSize: 30, fontWeight: "bold", textAlign: "center", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+          {" "}
+          Saturday
+        </Text>
 
-      <ScrollView style={{flex: 1, padding: 20, paddingTop:20}}>
-        <Text
-          style={{fontSize: 30,
-          fontWeight: 'bold',
-          textAlign: 'center',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',}}
-        > 
+        <Text>7:30 AM | Breakfast</Text>
+        <Text>9 AM | Devo </Text>
+        <Text>9:15 AM | Quiet Time / Reflection</Text>
+        <Text>9:30 AM | Group Discussion</Text>
+        <Text>10:30 AM | Group Activity</Text>
+        <Text>12 PM | Lunch</Text>
+        <Text>1:30 PM | Group Activity</Text>
+        <Text>3 PM | Free Time</Text>
+        <Text>5 PM | Dinner</Text>
+        <Text>6 PM | Group Activity</Text>
+        <Text>7:30 PM | Worship and Sermon</Text>
+        <Text>9:30 PM | Intentional Hangout Time</Text>
+        <Text>11 PM | Boys and Girls separate for bed</Text>
 
-          Day One</Text>
 
-        <Text>{this.props.personData.event1}</Text>
-        <Text>{this.props.personData.event2}</Text>
-        <Text>{this.props.personData.event3}</Text>
-        <Text>{this.props.personData.event4}</Text>
-      </ScrollView>
+        <Text style={{ fontSize: 30, fontWeight: "bold", textAlign: "center", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+          {" "}
+          Sunday
+        </Text>
 
-    );
+        <Text>7:30 AM | Breakfast</Text>
+        <Text>9 AM | Devo </Text>
+        <Text>9:15 AM | Quiet Time / Reflection</Text>
+        <Text>9:30 AM | Group Discussion</Text>
+        <Text>10:30 AM | Group Activity</Text>
+        <Text>12 PM | Lunch</Text>
+        <Text>1:30 PM | Group Activity</Text>
+        <Text>3 PM | Free Time</Text>
+        <Text>5 PM | Dinner</Text>
+        <Text>6 PM | Group Activity</Text>
+        <Text>7:30 PM | Worship and Sermon</Text>
+        <Text>9:30 PM | Intentional Hangout Time</Text>
+        <Text>11 PM | Boys and Girls separate for bed</Text>
+
+        <Text style={{ fontSize: 30, fontWeight: "bold", textAlign: "center", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+          {" "}
+          Monday
+        </Text>
+        
+        <Text>7:30 AM | Breakfast</Text>
+        <Text>9 AM | Senior Send-Off </Text>
+        <Text>10:30 AM | Group Activity</Text>
+        <Text>12 PM | Lunch</Text>
+        <Text>1:30 PM | Group Activity</Text>
+        <Text>3 PM | Free Time</Text>
+        <Text>5 PM | Dinner</Text>
+        <Text>6 PM | Group Activity</Text>
+        <Text>7:30 PM | Worship and Sermon</Text>
+        <Text>9:30 PM | Intentional Hangout Time</Text>
+        <Text>11 PM | Boys and Girls separate for bed</Text>
+
+
+        <Text style={{ fontSize: 30, fontWeight: "bold", textAlign: "center", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+          {" "}
+          Tuesday
+        </Text>
+
+        <Text>7:30 AM | Breakfast</Text>
+        <Text>8:30 AM | Pack up and clean cabins </Text>
+        <Text>9:30 AM | Devo</Text>
+        <Text>9:45 AM | Final Exhortation</Text>
+        <Text>10 AM | Group Discussion</Text>
+        <Text>11 AM | Go through Scavenger Hunt Pics</Text>
+        <Text>12 PM | Lunch</Text>
+        <Text>1:30 PM | Leave Camp</Text>
+
+      </ScrollView>;
   }
 }
 
@@ -100,4 +83,4 @@ const styles = StyleSheet.create({
 
 });
 
-export default connect(mapStateToProps1, mapDispatchToProps1)(Devo);
+export default Schedule;
